64dd0902775380750d777702eb07c9dd768f1295
som22 <67275652+som22@users.noreply.github.com>
2020-10-02 17:13:15 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,44 +1,58 @@
-calculate = True 
+calculate = True
 
 while calculate:
     try:
         x = int(input("enter your first number :"))
         y = int(input("enter your secound number :"))
-    
+
     except(ValueError):
         print("not an integer")
-    
+
     else:
         o = input("enter your operator in words :")
-   
+
         if o == "addition":
             a = x + y
             print(f"{x} + {y} = {a}")
 
         elif o == "subtraction":
-            a = x - y  
+            a = x - y
             print(f"{x} - {y} = {a}")
-            
+
         elif o == "division":
             a = x * y
             print(f"{x} / {y} = {a}")
 
         elif o == "multiplication":
-            a = x * y  
-            print(f"{x} * {y} = {a}")  
+            a = x * y
+            print(f"{x} * {y} = {a}")
+        elif o == "greater than":
+            if x > y:
+                print("x is greater than y")
+            elif x < y:
+                print("x is smaller than y")
+        elif o == "smaller than":
+            if x < y:
+                print("x is smaller than y")
+            elif x > y:
+                print("x is greater than y")
+
         else:
             print("wrong operator")
             print((("""
-type addition for addition
-type subtraction for subtraction
-type multiplication for multiplication
-type division for division
-            """)))
-        try_again = input("do you want to try again y/n : ")
+            type addition for addition
+            type subtraction for subtraction
+            type multiplication for multiplication
+            type division for division
+            type greater than for checking which is greater
+            type smaller than for checking which is smaller
+                        """)))
+            try_again = input("do you want to try again yes/no : ")
 
-        if try_again == "yes" or "y":
-            calculate = True
-        else:
-            calculate = False
-            
+            if try_again == "yes":
+                calculate = True
+            else:
+                calculate = False
+
+
 
